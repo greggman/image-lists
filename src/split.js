50e089235af7d8ff1d6a395a5f4ca1519ebb24d7
Gregg Tavares <github@greggman.com>
2023-05-18 16:04:28 +0900

    build in out

--- src/split.js
+++ src/split.js
@@ -2,13 +2,14 @@ import fs from 'fs';
 import path from 'path';
 
 async function main() {
+  fs.mkdirSync('out', { recursive: true });
   const images = JSON.parse(fs.readFileSync('image-list.json', {encoding: 'utf-8'}));
 
   {
     let js = fs.readFileSync('src/get-random-image-template.js', {encoding: 'utf-8'});
     js = js.replace('numImages = 0', `numImages = ${images.length}`);
-    fs.writeFileSync('get-random-image.js', js);
-    fs.writeFileSync('num-images.json', JSON.stringify({numImages: images.length}));
+    fs.writeFileSync('out/get-random-image.js', js);
+    fs.writeFileSync('out/num-images.json', JSON.stringify({numImages: images.length}));
     console.log('num images:', images.length);
     console.log('num files:', (images.length + 49) / 50 | 0);
   }
@@ -19,7 +20,7 @@ async function main() {
     const sliceId = (i / numPerSlice).toString().padStart(5, '0');
     const parts = /^(\d+)(\d)(\d)(\d)(\d)$/.exec(sliceId).slice(1);
     const file = `${parts.pop()}.json`;
-    const filename = path.join('images', ...parts, file);
+    const filename = path.join('out', 'images', ...parts, file);
     fs.mkdirSync(path.dirname(filename), {recursive: true});
     fs.writeFileSync(filename, JSON.stringify(slice));
   }
